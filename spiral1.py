7643a2425fba74d924dfcc319c673f5c322f81a2
rnb7880 <rnb7880@rit.edu>
2017-01-10 20:41:20 -0500

    blinking light

--- spiral1.py
+++ spiral1.py
@@ -13,27 +13,46 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("SpiralWave")
 clock = pygame.time.Clock()
 
+excite = pygame.USEREVENT + 1
+
+pygame.time.set_timer(excite, 1000)
+
+
 coins = [[0 for i in range(10)] for j in range(10)]
 for y in range (10):
     for x in range (10):
         coins[y][x] = Coin(x*50 + 10,y*50 + 10)
+
 
 for line in coins:
     for coin in line:
 
         for addline in coins:
             for addcoin in addline:
-                if m.fabs(coin.x - addcoin.x) == 1:
-                    coin.add(addcoin)
+                currx = coin.x
+                curry = coin.y
+                newx = addcoin.x
+                newy = addcoin.y
+                if  not (coin.x == addcoin.x and addcoin.y == coin.y):
+                    if abs(coin.x - addcoin.x) <= 50 and abs(coin.y - addcoin.y) <= 50:
+                        coin.add(addcoin)
 
 
 while not over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             over = True
+        if event.type == excite:
+            coins[5][5].excite()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
+                #coins[5][5].chain()
                 coins[5][5].excite()
+                # print('red')
+                # pygame.time.wait(1000)
+                # print('wait')
+                # coins[5][5].release()
+                # print('green')
 
     for line in coins:
         for coin in line:
--- spiralClasses.py
+++ spiralClasses.py
@@ -17,9 +17,14 @@ class Coin():
         """
         uses pygame and the global screen variable to draw the building on the screen
         """
-        self.color = (255, 0, 0)
-        self.alert()
-        #self.release()
+        if self.color == (0,255,0):
+            self.color = (255,0,0)
+        else:
+            self.color = (0, 255, 0)
+
+    def chain(self):
+        self.excite()
+
 
     def release(self):
         """
@@ -33,7 +38,6 @@ class Coin():
         pygame.draw.circle(screen,self.color,(self.x,self.y),10)
     def add(self,coin):
         self.adjacent.append(coin)
-        print(self.adjacent)
     def alert(self):
         for coin in self.adjacent:
             if coin.color == (255,0,0):
